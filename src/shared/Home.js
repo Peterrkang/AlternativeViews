0288b51b847c954cad3a9e1f6253b5add356d865
Peter Kang <peterrkang@gmail.com>
2018-10-24 16:29:18 -0700

    add search bar on home page

--- src/shared/Home.js
+++ src/shared/Home.js
@@ -1,10 +1,40 @@
-import React, { Component } from "react";
-import withStyles from "isomorphic-style-loader/lib/withStyles";
-import styles from "./styles.css";
+import React, { Component } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './styles.css';
+import { Route } from 'react-router-dom';
 
 class Home extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleInputChange(e) {
+    e.preventDefault();
+    this.setState({ inputValue: e.target.value });
+  }
+
   render() {
-    return <div className={styles.home}>Home</div>;
+    return (
+      <Route
+        render={({ history }) => (
+          <React.Fragment>
+            <form
+              onSubmit={() => history.push(`/topics/${this.state.inputValue}`)}
+            >
+              <label>
+                Search News Articles:
+                <input
+                  type="text"
+                  value={this.state.inputValue}
+                  onChange={e => this.handleInputChange(e)}
+                />
+              </label>
+              <input type="submit" value="Search Topic" />
+            </form>
+          </React.Fragment>
+        )}
+      />
+    );
   }
 }
 
